e2154b2d077e45ab8b47f7308432cb9c53e83011
kunal agarwal <akunal259@gmail.com>
2025-03-06 11:57:18 +0530

    bill generation

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, registerDealer, login } from "../controllers/authcontroller.js";
+import { registerUser, registerDealer, login } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
--- backend/routes/dealer.routes.js
+++ backend/routes/dealer.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { getDealerProfile, updateDealerProfile } from "../controllers/dealercontroller.js";
-import { verifyToken } from "../controllers/authcontroller.js";
+import { getDealerProfile, updateDealerProfile } from "../controllers/dealer.controller.js";
+import { verifyToken } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
