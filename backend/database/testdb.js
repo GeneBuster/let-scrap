24240035bac97bafb4b7f5d2bda6f937da851cf1
pri2003yam <132195934+pri2003yam@users.noreply.github.com>
2025-09-12 11:13:42 +0530

    Update testdb.js

--- backend/database/testdb.js
+++ backend/database/testdb.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 
-const mongoURI = 'mongodb+srv://akunal259:P16AZHtnnl14nQYf@cluster1.ot7g5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1'; // Replace with your MongoDB connection string
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
